eee64103919262585343786d5544e16ab1c6e3cf
qwert2603 <qwert2603@mail.ru>
2020-11-29 16:43:22 +0700

    icons

--- app/src/main/java/com/qwert2603/eten/presentation/screen/ScreenMain.kt
+++ app/src/main/java/com/qwert2603/eten/presentation/screen/ScreenMain.kt
@@ -1,16 +1,14 @@
 package com.qwert2603.eten.presentation.screen
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Text
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Place
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.vector.VectorAsset
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.*
 import com.qwert2603.eten.R
@@ -19,25 +17,25 @@ import com.qwert2603.eten.Route
 private enum class BottomMenuItem(
     val route: Route,
     @StringRes val labelRes: Int,
-    val icon: VectorAsset,
+    @DrawableRes val iconRes: Int,
     val content: @Composable () -> Unit
 ) {
     Meals(
         route = Route.Meals,
         labelRes = R.string.bottom_menu_meals,
-        icon = Icons.Default.List,
+        iconRes = R.drawable.ic_meal,
         content = { ScreenMealsList() }
     ),
     Dishes(
         route = Route.Dishes,
         labelRes = R.string.bottom_menu_dishes,
-        icon = Icons.Default.Menu,
+        iconRes = R.drawable.ic_dish,
         content = { ScreenDishesList() }
     ),
     Products(
         route = Route.Products,
         labelRes = R.string.bottom_menu_products,
-        icon = Icons.Default.Place,
+        iconRes = R.drawable.ic_product,
         content = { ScreenProductsList() }
     ),
 }
@@ -64,7 +62,7 @@ fun ScreenMain(
                 val currentRoute = navBackStackEntry?.arguments?.getString(KEY_ROUTE)
                 BottomMenuItem.values().forEach {
                     BottomNavigationItem(
-                        icon = { Icon(it.icon) },
+                        icon = { Icon(imageResource(it.iconRes)) },
                         label = { Text(stringResource(it.labelRes)) },
                         selected = currentRoute == it.route.name,
                         onClick = {
