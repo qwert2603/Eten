b55ba41d319753e12b18373c5fdda94eb57b6100
qwert2603 <qwert2603@mail.ru>
2020-12-26 23:38:22 +0700

    meals_list_is_empty text

--- app/src/main/java/com/qwert2603/eten/presentation/screen/lists/ScreenMealsList.kt
+++ app/src/main/java/com/qwert2603/eten/presentation/screen/lists/ScreenMealsList.kt
@@ -1,11 +1,21 @@
 package com.qwert2603.eten.presentation.screen.lists
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.viewModel
+import com.qwert2603.eten.R
 import com.qwert2603.eten.domain.model.Meal
 import com.qwert2603.eten.presentation.EtenViewModel
 import com.qwert2603.eten.presentation.list_item.ItemEtenDay
@@ -16,13 +26,26 @@ fun ScreenMealsList(
     navigateToEditMeal: (uuid: String) -> Unit,
 ) {
     val vm = viewModel<EtenViewModel>()
-    val meals = vm.etenDaysUpdates.collectAsState(initial = emptyList())
+    val etenDays by vm.etenDaysUpdates.collectAsState(initial = emptyList())
     var mealToDelete by remember { mutableStateOf<Meal?>(null) }
 
+    if (etenDays.isEmpty()) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Text(
+                stringResource(R.string.meals_list_is_empty),
+                style = TextStyle(fontSize = 26.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier,
+            )
+        }
+    }
     LazyColumn(
         contentPadding = PaddingValues(bottom = 112.dp),
     ) {
-        items(meals.value) { etenDay ->
+        items(etenDays) { etenDay ->
             ItemEtenDay(
                 etenDay = etenDay,
                 onMealClick = { navigateToEditMeal(it.uuid) },
