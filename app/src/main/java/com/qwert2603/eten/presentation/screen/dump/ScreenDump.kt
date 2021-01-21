6328a2be041fff6a1b64481ec5e61c24706070b7
qwert2603 <qwert2603@mail.ru>
2021-01-21 20:52:03 +0500

    don't show snackbar after dump restored -- crash

--- app/src/main/java/com/qwert2603/eten/presentation/screen/dump/ScreenDump.kt
+++ app/src/main/java/com/qwert2603/eten/presentation/screen/dump/ScreenDump.kt
@@ -41,8 +41,9 @@ fun ScreenDump(
             .launchIn(this)
         vm.dumpRestoredEvents
             .onEach {
-                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
-                scaffoldState.snackbarHostState.showSnackbar(dumpRestoredMessage)
+//                scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+//                scaffoldState.snackbarHostState.showSnackbar(dumpRestoredMessage)
+                navigateUp()
             }
             .launchIn(this)
     }
